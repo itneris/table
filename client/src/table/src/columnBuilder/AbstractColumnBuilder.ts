4af6bfa346a7858f19b24ec97103f41b7e32f2ed
jediar <jediar@yandex.ru>
2022-09-22 12:32:26 +0300

    Фикс парсинга ColumnBuilder

--- client/src/table/src/columnBuilder/AbstractColumnBuilder.ts
+++ client/src/table/src/columnBuilder/AbstractColumnBuilder.ts
@@ -6,7 +6,12 @@ export abstract class AbstractColumnBuilder<T extends LooseObject> {
 	private _columns: Array<ColumnDescription> = [];
 
 	public ColumnFor(model: (type: T) => any): ColumnOptionsBuilder<T> {
-		const key = model.toString().split(".")[1].replace("}", "");
+		const key = model.toString().split(".")[1]
+			.replace("}", "")
+			.replace("\n", "")
+			.replace("\r", "")
+			.replace(";", "")
+			.replace(/\s/g, "");
 		const column = new ColumnDescription();
 		column.order = this._columns.length;
 		column.property = key;
