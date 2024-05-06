db018af228a22398bdce4b8a9e7ec51b6cd8fb13
jediar <jediar@yandex.ru>
2024-05-06 18:54:06 +0200

    Export FilterValueProperties

--- client/src/table/src/index.ts
+++ client/src/table/src/index.ts
@@ -5,8 +5,9 @@ import { AbstractColumnBuilder } from "./columnBuilder/AbstractColumnBuilder";
 import "./columnBuilder/DefaultOptionsExtensions";
 import { ITableRef } from './base/ITableRef';
 import { FilterType } from './props/FilterType';
+import { FilterValueProperties } from './props/FilterValueProperties';
 
 export default ItnTable;
-export { FilterProperties, AbstractColumnBuilder, ColumnFilteringProperties, FilterType };
+export { FilterProperties, AbstractColumnBuilder, ColumnFilteringProperties, FilterType, FilterValueProperties };
 
 export type { ITableRef };
